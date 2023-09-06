b7f4ba4c0135957e4f9bc616e2adc0e81ea65f6c
bricteux <144225461+bricteux@users.noreply.github.com>
2023-09-06 14:41:32 +0200

    previous website commit

---
+++ People_files/People.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170616
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('People_files/PeopleMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
